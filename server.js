95a2af35272d01dcdf59c99fe0ea22778746e4be
Watxher <104895198+watxherthedumbass@users.noreply.github.com>
2023-12-18 13:18:07 +0800

    Update server.js

--- server.js
+++ server.js
@@ -1,10 +1,19 @@
 const express = require('express');
 const app = express();
+const sql = require('mysql');
+const parser = require('body-parser');
+const encoded = parser.urlencoded( {extended: false} );
 app.use(express.static('site'))
 
-app.get('/', function (req, res) {
-res.send("hello world");
+app.get('/index.html', function (req, res) {
+res.sendFile(__dirname + "/" + "index.html");
 });
+
+app.post("/", encoded, function (req, res) {
+ console.log(`name: ${req.body.name}, pass: ${req.body.pass}`);
+res.send("succesfull");
+});
+
 
 var server = app.listen(8081, function () {
    var host = server.address().address
