588d833a0408204499ccc55a7dc644836c4f1222
Watxher <104895198+watxherthedumbass@users.noreply.github.com>
2023-12-18 12:44:54 +0800

    Update server.js

--- server.js
+++ server.js
@@ -1,1 +1,14 @@
-const 
+const express = require('express');
+const app = express;
+app.use(express.static('shop'))
+
+app.get('/', function (req, res) {
+res.send("hello world");
+});
+
+var server = app.listen(8081, function () {
+   var host = server.address().address
+   var port = server.address().port
+
+   console.log("Example app listening at http://%s:%s", host, port)
+})
