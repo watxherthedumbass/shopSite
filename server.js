57b6969aa8680a9881ee43984995eaaefc56690c
Watxher <104895198+watxherthedumbass@users.noreply.github.com>
2023-12-18 12:46:43 +0800

    Update server.js

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express;
-app.use(express.static('shop'))
+app.use(express.static('site'))
 
 app.get('/', function (req, res) {
 res.send("hello world");
